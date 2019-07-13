7f88525ac4c8872762ecef24c9befdfaec80cbbe
maxyee <eyaminkhan00@gmail.com>
2019-07-14 01:25:33 +0600

    Added the inital api code

---
+++ spondon/Controller/API/TokenController.cs
@@ -0,0 +1,7 @@
+namespace spondon.Controller.API
+{
+    public class TokenController
+    {
+        
+    }
+}
--- spondon/Controller/API/UserController.cs
+++ spondon/Controller/API/UserController.cs
@@ -29,5 +29,50 @@ namespace spondon.Controller.API
 
             return Ok("hello world");
         }
+
+
+        // [HttpGet("register/{x}")]
+        // //[AllowAnonymous]
+        // //[Route("register/{x}")]
+        // public ActionResult<IEnumerable<string>> TestingApi(int x)
+        // {
+        //     try
+        //     {
+        //         int result = x;
+        //         return x;
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         return ex;
+        //     }
+
+        //     //return Ok("hello world");
+        // }
+
+        [HttpGet("register/{x}")]
+        public ActionResult<string> Fuck(int x)
+        {
+            int fuckX = x;
+            String fuckConvert = x.ToString();
+            return fuckConvert ;
+            //return new string[] {"fuck calling","fucking value"};
+        } 
+
+
+        //GET api/user
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"value1","value2"};
+        } 
+
+
+        //GET api/user/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
     }
+
+
 }
